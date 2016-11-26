550ca7473a8daab48c3656b63684e9e2aa70938d
Peter Temple <peter1403@hotmail.co.uk>
2016-11-26 21:17:48 +0000

    Image fix

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,6 +7,6 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :path_style            => true
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  "https://final.sample.ruby.app.image.upload.s3-website-eu-west-1.amazonaws.com"
   end
 end
